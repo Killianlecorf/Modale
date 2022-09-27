f6449b7053ed10f5a36072890c181fdc0e02d877
Killianlecorf <92156535+Killianlecorf@users.noreply.github.com>
2022-09-27 16:11:00 +0200

    Pop-up javascript

---
+++ popup/script.js
@@ -0,0 +1,8 @@
+var functionOpen = function Ope() {
+    document.getElementById("popup").style.display = "block";
+    document.getElementById("back").style.display = "block";
+  }
+var functionClose = function close() {
+    document.getElementById("popup").style.display = "none" ;
+    document.getElementById("back").style.display = "none";
+}
